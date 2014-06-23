4e3234f791769d5a3b5b9a78734c1befbe80ccb5
dennis84 <d.dietrich84@gmail.com>
2014-06-23 20:44:30 +0200

    ...

--- javascripts/index.js
+++ javascripts/index.js
@@ -1,8 +1,21 @@
-$(function() {
+function load(username, repo) {
+  // Make sections
   $('body > :first-child,h1,h2').each(function() {
     $(this)
       .nextUntil("h1,h2")
       .andSelf()
       .wrapAll('<section><div class="container"></section>')
   })
-})
+
+  // Make buttons
+  var section = document.querySelector('section:first-child')
+    , button = document.createElement('a')
+    , wrapper = document.createElement('div')
+
+  button.setAttribute('href', 'http://github.com/' + username + '/' + repo)
+  button.setAttribute('class', 'btn btn-default')
+  button.innerHTML = 'View on GitHub'
+  wrapper.setAttribute('class', 'text-center buttons')
+  wrapper.appendChild(button)
+  section.querySelector('.container').appendChild(wrapper)
+}
