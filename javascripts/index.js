884a6b11fc03f040e63d163a3a4a23f32b9c1d62
dennis84 <d.dietrich84@gmail.com>
2014-07-06 16:20:32 +0200

    new design

--- javascripts/index.js
+++ javascripts/index.js
@@ -1,21 +1,13 @@
 function load(username, repo) {
-  // Make sections
-  $('body > :first-child,h1,h2').each(function() {
-    $(this)
-      .nextUntil("h1,h2")
-      .andSelf()
-      .wrapAll('<section><div class="container"></section>')
-  })
+  // // Make buttons
+  // var section = document.querySelector('section:first-child')
+  //   , button = document.createElement('a')
+  //   , wrapper = document.createElement('div')
 
-  // Make buttons
-  var section = document.querySelector('section:first-child')
-    , button = document.createElement('a')
-    , wrapper = document.createElement('div')
-
-  button.setAttribute('href', 'http://github.com/' + username + '/' + repo)
-  button.setAttribute('class', 'btn btn-default')
-  button.innerHTML = 'View on GitHub'
-  wrapper.setAttribute('class', 'text-center buttons')
-  wrapper.appendChild(button)
-  section.querySelector('.container').appendChild(wrapper)
+  // button.setAttribute('href', 'http://github.com/' + username + '/' + repo)
+  // button.setAttribute('class', 'btn btn-default')
+  // button.innerHTML = 'View on GitHub'
+  // wrapper.setAttribute('class', 'text-center buttons')
+  // wrapper.appendChild(button)
+  // section.querySelector('.container').appendChild(wrapper)
 }
